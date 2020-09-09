be4437dc8754ecc604ed173d6664d2e08ef4ca27
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-09-08 20:22:53 -0600

    Write custom #attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) { self.instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |set_val| self.instance_variable_set("@#{name}", set_val) }
+    end
   end
 end
