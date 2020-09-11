a621a508e2c430ebc52f76ed4d387ce7e6634734
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-09-11 17:06:57 -0600

    Rspec 1a to 1d completed

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,18 +5,31 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if !@columns.nil?
+    data = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    col_names = data[0].map!(&:to_sym)
+    @columns = col_names
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) { self.attributes[name] }
+      define_method("#{name}=") { |value| self.attributes[name] = value }
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    return @table_name if !@table_name.nil?
+    self.name.tableize
   end
 
   def self.all
@@ -32,11 +45,19 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+   params.each do |attr_name, value|
+      # make sure to convert keys to symbols
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
