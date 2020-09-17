8bc3c64b83349aeac7a9e0091ed16b9d38909849
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-09-16 19:45:17 -0600

    Set up default attributes for belongs_to, has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -20,13 +20,30 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    init_vals = {
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id,
+      :foreign_key => "#{name}_id".to_sym
+    }
+
+    init_vals.keys.each do |key|
+      self.send("#{key}=",options[key] || init_vals[key])
+    end
+    
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    init_vals = {
+      :class_name => name.singularize.camelcase,
+      :primary_key => :id,
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym
+    }
+
+    init_vals.keys.each do |key|
+      self.send("#{key}=",options[key] || init_vals[key])
+    end    
   end
 end
 
