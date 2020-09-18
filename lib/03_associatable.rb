d6f7ad5e17652579f55f1bed1e3859860960dffa
CJ Gilroy <c.gilroy9@gmail.com>
2020-09-18 12:57:43 -0600

    Finish #has_many and #belongs_to associations

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -36,7 +36,7 @@ end
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     init_vals = {
-      :class_name => name.singularize.camelcase,
+      :class_name => name.to_s.singularize.camelcase,
       :primary_key => :id,
       :foreign_key => "#{self_class_name.underscore}_id".to_sym
     }
@@ -52,12 +52,16 @@ module Associatable
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name,options)
     define_method(name) do
-      options.model_class.where(options.primary_key => self.send(options.foreign_key))
+      options.model_class.where(options.primary_key => self.send(options.foreign_key)).first
     end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name,self.name,options)
+    define_method(name) do
+      options.model_class
+        .where(options.foreign_key => self.send(options.primary_key))
+    end
   end
 
   def assoc_options
